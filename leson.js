5b36316d8f5088e816999ecc650ef5a6687dc0eb
loke <1009483976@qq.com>
2020-06-10 14:26:03 +0800

    基本做完

--- leson.js
+++ leson.js
@@ -6,11 +6,14 @@ import Video from 'react-native-video'
 import { createStackNavigator } from '@react-navigation/stack';
 import Entypo from "react-native-vector-icons/Entypo"
 import FontAwesome from "react-native-vector-icons/FontAwesome"
+import Slider from '@react-native-community/slider'
+
 const Stack = createStackNavigator()
 var p = 0
 export default class yq extends Component {
     constructor() {
         super()
+        this.player = null
         this.state = {
             playlist: true,
             name: 'controller-play',
@@ -18,7 +21,9 @@ export default class yq extends Component {
             music: '',
             singer: '蒲提-爱我 (男生版)',
             img: 'http://118.25.250.106/qypicture/aiwopic.jpg',
-            size:50
+            size: 50,
+            duration: 1,
+            currentTime: 0
         }
     }
 
@@ -32,7 +37,7 @@ export default class yq extends Component {
             singer: singerPath,
             img: imgPath,
             name: 'control-play',
-            playlist: true
+            playlist: true,
         })
     }
 
@@ -61,21 +66,53 @@ export default class yq extends Component {
             playlist: true
         })
     }
+    _loadHandler = ({ duration }) => {
+        this.setState({
+            duration
+        })
+    }
+    _progressHandler = ({ currentTime }) => {
+        this.setState({
+            currentTime
+        })
+    }
+    _seekHandler = (value) => {
+        this.player.seek(value)
+    }
+    _endHandler=()=>{
+        this.next()
+    }
     render() {
         return (
             <View>
                 <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
                     <Image style={{ width: '80%', height: 300, borderRadius: 18 }} source={{ uri: this.props.route.params.imagePath }} />
                 </View>
-                <View style={{ height: 180, margin: 10 ,paddingLeft:26 , marginTop:20 , justifyContent:'center',alignItems:'center',marginRight:50}}>
+                <View style={{ height: 180, margin: 10, paddingLeft: 26, marginTop: 20, justifyContent: 'center', alignItems: 'center', marginRight: 50 }}>
                     <FontAwesome style={{ marginBottom: 10, marginRight: 15 }} name="optin-monster" size={23} color="black" />
-                    <Text style={{ fontSize: 20  }}>{this.props.route.params.name}</Text>
+                    <Text style={{ fontSize: 20 }}>{this.props.route.params.name}</Text>
                 </View>
                 {/* optin-monster */}
-                <View style={{alignItems:'center'}}>
+                <View>
+                    <Slider
+                        style={{ width: '90%', marginLeft: 20 }}
+                        minimumValue={0}
+                        maximumValue={this.state.duration}
+                        value={this.state.currentTime}
+                        onSlidingComplete={this._seekHandler}
+                    ></Slider>
+                </View>
+                <View style={{ alignItems: 'center' ,marginLeft:15}}>
                     <View style={{ flexDirection: "row", alignItems: "center" }}>
 
-                        <Video source={{ uri: this.state.music }} paused={this.state.playlist}></Video>
+                        <Video
+                            ref={ref => this.player = ref}
+                            source={{ uri: this.state.music }}
+                            paused={this.state.playlist}
+                            onLoad={this._loadHandler}
+                            onProgress={this._progressHandler}
+                            onEnd={this._endHandler}
+                        ></Video>
 
                         <TouchableHighlight underlayColor="none" activeOpacity={0.1}
                             onPress={this.last}>
